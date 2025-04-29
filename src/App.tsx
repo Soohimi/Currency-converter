b01e4cb4fa07ed3f4999510474ec619b682a4cde
Soohimi <soohimi2003@gmail.com>
2025-04-29 14:34:29 +0330

    adding new api

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import CurrencyRow from './components/CurrencyRow';
 
+const BASE_URL = 'https://latest.currency-api.pages.dev/v1/currencies/eur.json'
 
 function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  console.log(currencyOptions)
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => {
+        const rates = data.eur;
+        setCurrencyOptions(...Object.keys(rates));
+      });
+  }, []);
+  
   return (
     <>
       <h1>Convert</h1>
-      <CurrencyRow />
+      <CurrencyRow 
+        currencyOptions={currencyOptions}
+      />
       <div className='equals'>=</div>
-      <CurrencyRow />
+      <CurrencyRow 
+        currencyOptions={currencyOptions}
+      />
     </>
   );
 }
